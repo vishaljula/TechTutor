1419b7f2cdb008d2df69142af0b348971848b2c1
Aditya Vishal <vishal.jula@gmail.com>
2017-07-18 01:02:48 -0400

    Created basic react components for home and about pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return(
+      <div className="jumbotron">
+        <h1>About</h1>
+        <p>This app uses react, redux and all other cool trending technologies out there</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
